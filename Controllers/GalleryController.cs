72772346f9cc352e011d26293e445b0396c411a9
Paula <paula.hercigonja@fer.hr>
2019-06-03 02:10:14 +0200

    Gallery

--- Controllers/GalleryController.cs
+++ Controllers/GalleryController.cs
@@ -1,14 +1,44 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DanceClubs.Data;
+using DanceClubs.Data.Models;
+using DanceClubs.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace DanceClubs.Controllers
 {
     //[Authorize(Roles = "User")]
     public class GalleryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IRepository _repository;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public GalleryController(ApplicationDbContext context, IRepository repository, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _repository = repository;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var userId = _userManager.GetUserId(User);
+
+            var groupUsers = _repository.GetGroupUsersByUserId(userId);
+            var images = groupUsers.SelectMany(i => _repository.GetImagesByGroupId(i.GroupId)).ToList();
+            var model = new ImageIndexModel
+            {
+                Images = images.Select(i => new ImageListingModel
+                {
+                    Author = i.Author.UserName,
+                    GroupName = i.Group.Name,
+                    Published = i.Published,
+                    Url = i.ImageUrl
+                }).ToList()
+            };
+            return View(model);
         }
     }
 }
