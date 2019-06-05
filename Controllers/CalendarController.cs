45d3671251b2b41fe8a13e05c4a9a958decf30b0
Paula <paula.hercigonja@fer.hr>
2019-06-05 22:48:39 +0200

    iCalendar available as a download

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -2,14 +2,11 @@
 using DanceClubs.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Ical.Net;
-using Ical.Net.CalendarComponents;
 using System.Linq;
-using Ical.Net.DataTypes;
 using System;
-using Microsoft.AspNetCore.Authorization;
-using Ical.Net.Serialization;
 using DanceClubs.Models;
+using System.Text;
+using System.IO;
 
 namespace DanceClubs.Controllers
 {
@@ -60,7 +57,44 @@ namespace DanceClubs.Controllers
             });
 
             return Json(events.ToArray());            
-        }        
+        }
+        
+        public ActionResult DownloadiCal()
+        {
+            var userId = _userManager.GetUserId(User);
+
+            var groupUsers = _repository.GetGroupUsersByUserId(userId);
+            var activities = groupUsers.SelectMany(i => _repository.GetActivitiesByGroupId(i.GroupId)).ToList();
+            StringBuilder sb = new StringBuilder();
+            string DateFormat = "yyyyMMddTHHmmssZ";
+            string now = DateTime.Now.ToUniversalTime().ToString(DateFormat);
+            sb.AppendLine("BEGIN:VCALENDAR");
+            sb.AppendLine("PRODID:");
+            sb.AppendLine("VERSION:2.0");
+            sb.AppendLine("METHOD:PUBLISH");
+            foreach (var activity in activities)
+            {                
+                DateTime dtStart = Convert.ToDateTime(activity.Start);
+                DateTime dtEnd = Convert.ToDateTime(activity.End);
+                sb.AppendLine("BEGIN:VEVENT");
+                sb.AppendLine("DTSTART:" + dtStart.ToUniversalTime().ToString(DateFormat));
+                sb.AppendLine("DTEND:" + dtEnd.ToUniversalTime().ToString(DateFormat));
+                sb.AppendLine("DTSTAMP:" + now);
+                sb.AppendLine("UID:" + Guid.NewGuid());
+                sb.AppendLine("CREATED:" + now);
+                sb.AppendLine("DESCRIPTION:" + activity.Group.Name + " - " + activity.ActivityType.Name);
+                sb.AppendLine("LAST-MODIFIED:" + now);
+                sb.AppendLine("LOCATION:" + activity.Location);
+                sb.AppendLine("SEQUENCE:0");
+                sb.AppendLine("STATUS:CONFIRMED");
+                sb.AppendLine("SUMMARY:" + activity.Group.Name + " - " + activity.ActivityType.Name);
+                sb.AppendLine("TRANSP:OPAQUE");
+                sb.AppendLine("END:VEVENT");                
+            }
+            sb.AppendLine("END:VCALENDAR");
+            var calendarBytes = Encoding.UTF8.GetBytes(sb.ToString());
+            return File(calendarBytes, "text/calendar", "calendar.ics");
+        }
 
     }
 }
